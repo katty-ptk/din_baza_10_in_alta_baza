11c9458495d9101b1716576cbdc5035b611ee7b4
Katty Lazar <katy.lazar05@gmail.com>
2020-10-07 22:34:11 +0300

    checking empty input + adding styles to error + non-error

--- js/app.js
+++ js/app.js
@@ -4,15 +4,23 @@ let result;
 $("#submit_btn").click(function(e) {
   e.preventDefault();
 
-  var number = $('input[id="input_nr"]').val();
-  var base = $('input[id="input_base"]').val();
+  var number = parseInt($('input[id="input_nr"]').val());
+  var base = parseInt($('input[id="input_base"]').val());
 
-  if ( isNaN( number ) || isNaN( base ) ) {
-    result_p.textContent = "te rog introdu un numar";
+  if ( !(isNaN(number)) && !(isNaN(base)) ) {
+
+    result_p.textContent = "ai introdus numere";
+    result_p.setAttribute('id', 'result_msg')
+
   } else {
-    result_p.textContent = "ai introud numere";
+
+    result_p.textContent = "te rog introdu numere";
+    result_p.setAttribute('id', 'error_msg');
+
   }
 
   $(".result").append(result_p);
-  console.log(number, base, result_p)
+  // console.log(number, base, result_p)
+
+  
 });
